962757f22f219490aee632f68cb30cbe547a1aef
Indranil Sinharoy <indranil_leo@yahoo.com>
2013-04-27 00:24:30 -0500

    Added License file, and changed license from BSD to MIT

--- Test/pyZDDEunittest.py
+++ Test/pyZDDEunittest.py
@@ -5,8 +5,8 @@
 # Author:      Indranil Sinharoy
 #
 # Created:     19/10/2012
-# Copyright:   (c) 2012
-# Licence:     BSD
+# Copyright:   (c) Indranil 2012, 2013
+# Licence:     MIT License
 #-------------------------------------------------------------------------------
 
 import os, sys, unittest
