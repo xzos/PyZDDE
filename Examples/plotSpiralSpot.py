fcfdfac90aa16a55e45adaf53fb504a60b2233a0
Indranil Sinharoy <indranil_leo@yahoo.com>
2013-05-29 15:28:24 -0500

    Added images for doc

--- Examples/plotSpiralSpot.py
+++ Examples/plotSpiralSpot.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 cd = os.path.dirname(os.path.realpath(__file__))
 ind = cd.find('Examples')
 cd = cd[0:ind-1]
-
+##
 if cd not in sys.path:
     sys.path.append(cd)
 # ************************************************************************
@@ -50,7 +50,8 @@ if ~status:
                 (xb,yb,zb,intensityb) = link0.spiralSpot(hx,hy,1,spirals,rays)
                 (xg,yg,zg,intensityg) = link0.spiralSpot(hx,hy,2,spirals,rays)
                 (xr,yr,zr,intensityr) = link0.spiralSpot(hx,hy,3,spirals,rays)
-                fig,ax = plt.subplots(1,1)
+                fig = plt.figure(facecolor='w')
+                ax = fig.add_subplot(111)
                 ax.set_aspect('equal')
                 ax.scatter(xr,yr,s=8,c='red',linewidth=0.5,zorder=20)
                 ax.scatter(xg,yg,s=8,c='lime',linewidth=0.5,zorder=21)
